612fba9b0437a6a6bf0e4444b0b92d14118219fd
Marcin Maslanka <maslankam92@gmail.com>
2020-12-03 07:58:52 +0100

    Add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
 import { GetStaticProps } from "next";
-import Link from "next/link";
-import { useColorMode, Button } from "@chakra-ui/react";
+import NextLink from "next/link";
+import {
+  useColorMode,
+  Button,
+  Box,
+  Flex,
+  IconButton,
+  Container,
+  Heading,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 import notion from "../utils/notion.service";
 
 export type Post = { id: string; slug: string; title: string };
+
+const StickyNav = styled(Flex)`
+  position: sticky;
+  z-index: 10;
+  top: 0;
+  backdrop-filter: saturate(180%) blur(20px);
+  transition: background-color 0.1s ease-in-out;
+`;
+
+const navBgColor = {
+  light: "rgba(255, 255, 255, 0.8)",
+  dark: "rgba(23, 25, 35, 0.8)",
+};
 
 export const getStaticProps: GetStaticProps = async () => {
   const table = await notion.getTable();
@@ -20,24 +45,63 @@ const HomePage = ({ posts }: { posts: Post[] }) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <div>
-      <h1>Welcome to my blog</h1>
-      <h2>These are my recent posts:</h2>
-      <header>
-        <Button onClick={toggleColorMode}>
-          Toggle {colorMode === "light" ? "Dark" : "Light"}
-        </Button>
-      </header>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={"/blog/[slug]"} as={`/blog/${post.slug}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <StickyNav
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        maxWidth="900px"
+        width="100%"
+        bg={navBgColor[colorMode]}
+        as="nav"
+        p={8}
+        mt={[0, 8]}
+        mb={8}
+        mx="auto"
+      >
+        <Box as="span">LOGO</Box>
+        <Flex>
+          <Box mr={2}>
+            <NextLink href="/blog" passHref>
+              <Button as="a" variant="ghost" p={[1, 4]}>
+                Blog
+              </Button>
+            </NextLink>
+            <NextLink href="/about" passHref>
+              <Button as="a" variant="ghost" p={[1, 4]}>
+                About
+              </Button>
+            </NextLink>
+          </Box>
+          <Box>
+            <IconButton
+              aria-label="Toggle dark mode"
+              icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+              onClick={toggleColorMode}
+            />
+          </Box>
+        </Flex>
+      </StickyNav>
+
+      <Container>
+        <Heading size="2xl" mb="4">
+          Welcome to my blog
+        </Heading>
+        <Heading size="xl" mb="4">
+          These are my recent posts:
+        </Heading>
+
+        <UnorderedList>
+          {posts.map((post) => (
+            <ListItem key={post.slug}>
+              <NextLink href={"/blog/[slug]"} as={`/blog/${post.slug}`}>
+                <a>{post.title}</a>
+              </NextLink>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Container>
+    </>
   );
 };
 
