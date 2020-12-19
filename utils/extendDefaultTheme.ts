e3f82d37cfee5c8eb291a041b48adbc368300a61
Marcin Maslanka <maslankam92@gmail.com>
2020-12-19 09:27:46 +0100

    Add styles for blog post elements

--- utils/extendDefaultTheme.ts
+++ utils/extendDefaultTheme.ts
@@ -20,6 +20,17 @@ const styles = {
     },
     ".notion-h2": {
       fontSize: ["3xl", "4xl"],
+      position: "relative",
+      display: "inline-block",
+    },
+    ".notion-h2:before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      background: props.colorMode === "dark" ? "purple.900" : "purple.100",
+      height: "18px",
+      bottom: "2px",
+      "z-index": "-10",
     },
     ".notion-h3": {
       fontSize: ["2xl", "3xl"],
@@ -32,6 +43,14 @@ const styles = {
       _hover: {
         textDecoration: "underline",
       },
+    },
+    ".notion-callout": {
+      padding: [3, 5],
+      borderRadius: "lg",
+      boxShadow: props.colorMode === "dark" ? "lgLight" : "md",
+    },
+    ".notion-callout .notion-emoji": {
+      display: "none",
     },
   }),
 };
